cc5fd659f330063bcbe8a264c35b6b239cfcbd00
TomLop11 <tomas.lopes@ua.pt>
2026-09-24 12:34:54 +0100

    Remove cache and node_modules. Add .gitignore

--- frontend/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/tomas-lopes/PEI/docs/frontend/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/tomas-lopes/PEI/docs/frontend/node_modules/infima/dist/css/default/default.css"),
-  require("/home/tomas-lopes/PEI/docs/frontend/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/tomas-lopes/PEI/docs/frontend/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/tomas-lopes/PEI/docs/frontend/src/css/custom.css"),
-];
